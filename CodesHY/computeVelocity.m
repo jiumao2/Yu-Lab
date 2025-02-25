eb4951fb6497c43433f85431236e61d1aa2bfc95
Yue Huang <806628409@qq.com>
2025-02-25 17:37:03 +0800

    2025.2.25

---
+++ CodesHY/computeVelocity.m
@@ -0,0 +1,25 @@
+function v = computeVelocity(x, y)
+% compute the velocity from x and y
+% do not accept NaN values
+
+if nargin < 2
+    y = zeros(size(x));
+end
+
+assert(all(~isnan(x)) && all(~isnan(y)));
+
+v_temp = sqrt((x(2:end) - x(1:end-1)).^2 + (y(2:end) - y(1:end-1)).^2);
+
+flag_transpose = false;
+if size(v_temp, 1) ~= 1
+    v_temp = v_temp';
+    flag_transpose = true;
+end
+
+v = mean([NaN, v_temp; v_temp, NaN], 'omitnan');
+
+if flag_transpose
+    v = v';
+end
+
+end
